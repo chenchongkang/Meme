8b720d6b291c8758940a298db41b2245992f3f46
chenchongkang <751547892@qq.com>
2018-03-19 21:19:22 +0800

    Initial commit

---
+++ src/main/java/com/chen/meme/MemeApplication.java
@@ -0,0 +1,12 @@
+package com.chen.meme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/chen/meme/server/test.java
@@ -0,0 +1,8 @@
+package com.chen.meme.server;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class test {
+
+}
