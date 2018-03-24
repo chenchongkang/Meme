a19396a01ae7e44c66233f08b00abe25076a42d7
chenchongkang <751547892@qq.com>
2018-03-24 22:46:58 +0800

    3

--- src/main/java/com/chen/meme/model/Entityuser.java
+++ src/main/java/com/chen/meme/model/Entityuser.java
@@ -1,6 +1,13 @@
 package com.chen.meme.model;
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Entityuser {
+    @Id
+    @GeneratedValue
     private Integer userID;
     private String userName;
     private String password;
@@ -9,4 +16,71 @@ public class Entityuser {
     private String avatar;
     private String qq;
     private String introduction;
+
+    public Entityuser() {
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
 }
